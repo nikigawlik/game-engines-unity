fd031afb3e7b317e9c1195a3f7af62951f2358c1
Nils Gawlik <nilsgawlik@gmx.de>
2018-10-18 16:13:44 +0200

    Add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private float force = 2f;
+
+	private Animator animator;
+	private Rigidbody2D rigidbody2d;
+
+	private void Start() {
+		animator = GetComponent<Animator>();
+		rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() {
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		input = input.normalized;
+		int d = (int)(Mathf.Round(Mathf.Atan2(input.y, input.x) / (2f * Mathf.PI)) * 4) % 4;
+
+		if(input.magnitude > 0.01) {
+			animator.SetFloat("moveX", input.x);
+			animator.SetFloat("moveY", input.y);
+
+			rigidbody2d.AddForce(input * force);
+		}
+	}
+}

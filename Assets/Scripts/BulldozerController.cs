21c91071dfc498eba1e2398d336b46ff93660458
Nils Gawlik <nilsgawlik@gmx.de>
2018-10-24 17:40:49 +0200

    Add bulldozer chase behaviour and game controller

---
+++ Assets/Scripts/BulldozerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulldozerController : MonoBehaviour {
+	[SerializeField] private float speed;
+	private Rigidbody2D rb;
+	private Animator anim;
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	private void FixedUpdate() {
+		PlayerController player = GameController.Instance.player;
+		if(player != null) {
+			rb.velocity = (player.transform.position - transform.position).normalized;
+		}
+	}
+
+	private void Update() {
+		anim.SetFloat("lookX", rb.velocity.x);
+		anim.SetFloat("speed", rb.velocity.magnitude);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public PlayerController player;
+
+	private static GameController instance;
+	public static GameController Instance {
+		get {
+			return instance;
+		}
+	}
+
+	private void Start() {
+		if(GameController.instance != null) {
+			GameObject.Destroy(gameObject);
+		} else {
+			GameController.instance = this;
+		}
+	}
+}

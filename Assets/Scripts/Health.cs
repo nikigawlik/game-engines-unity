62b4ddffcd43b3fa69ef2d5bbe717cc6c6717563
Georg Donner <georgdonner98@gmail.com>
2018-10-24 17:11:17 +0200

    Add Health component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	[SerializeField] private int max = 5;
+	[SerializeField] private bool isPlayer = false;
+	[SerializeField] private bool isInvincible = false;
+	[SerializeField] private float inivincibleDuration = 5f; 
+	private int current;
+	private SpriteRenderer sprite;
+
+	void Start () {
+		current = max;
+		sprite = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown("l")) {
+			ApplyDamage(2);
+		}
+	}
+
+	public void ApplyDamage (int n) {
+		if (!isInvincible) {
+			current -= n;
+			if (current <= 0) {
+				GameObject.Destroy(gameObject);
+			} else if (isPlayer) {
+				StartCoroutine(MakeInvincible());
+			}
+		}
+	}
+
+	public void Heal (int n) {
+		current = Mathf.Max(max, current + n);
+	}
+
+	private IEnumerator MakeInvincible() {
+		isInvincible = true;
+		Color objColor = sprite.color;
+		objColor.a = 0.3f;
+		sprite.color = objColor;
+		yield return new WaitForSeconds(inivincibleDuration);
+		isInvincible = false;
+		objColor.a = 1;
+		sprite.color = objColor;
+	}
+}

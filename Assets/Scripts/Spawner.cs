a55b1fc0d0b9d5ab4220c3dee7138dfa6b1b89ac
Georg Donner <georgdonner98@gmail.com>
2018-10-31 13:15:33 +0100

    Add enemy spawners to entrances.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject enemy;
+	public int frequency = 5;
+	
+	void Start () {
+		StartCoroutine(Spawn());
+	}
+
+	private IEnumerator Spawn() {
+		yield return new WaitForSeconds(frequency);
+		while (true) {
+			PlayerController player = GameController.Instance.player;
+			if (player != null) {
+				GameObject spawned = Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+				spawned.transform.SetParent(gameObject.transform);
+				yield return new WaitForSeconds(frequency);
+			} else {
+				break;
+			}
+		}
+	}
+}

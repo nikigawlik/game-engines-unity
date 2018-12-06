5937fea89da8e671ea09610eee94e519cd98e642
Nils Gawlik <nilsgawlik@gmx.de>
2018-12-06 16:05:08 +0100

    Add tree growth

---
+++ Assets/Scripts/SpriteChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour {
+	public Sprite[] sprites;
+	private int spriteIndex;
+	public int SpriteIndex {
+		get {
+			return spriteIndex;
+		}
+		set {
+			spriteIndex = Mathf.Clamp(value, 0, sprites.Length - 1);
+			SpriteRenderer sr = GetComponent<SpriteRenderer>();
+			if(sr) sr.sprite = sprites[spriteIndex];
+		}
+	}
+
+	[EasyButtons.Button]
+	public void Test() {
+		SpriteIndex++;
+	}
+}
